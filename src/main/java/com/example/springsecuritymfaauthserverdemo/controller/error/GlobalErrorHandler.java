e4e871dd63808d1b2cda6c5ce5f7800d98268306
ankit <ankitforstudy@gmail.com>
2021-10-08 16:59:41 +0530

    Service changes for adding, authentication, token validation and token generation

--- src/main/java/com/example/springsecuritymfaauthserverdemo/controller/error/GlobalErrorHandler.java
+++ src/main/java/com/example/springsecuritymfaauthserverdemo/controller/error/GlobalErrorHandler.java
@@ -1,8 +1,26 @@
 package com.example.springsecuritymfaauthserverdemo.controller.error;
 
+import com.example.springsecuritymfaauthserverdemo.controller.dto.AuthenticationResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.security.auth.login.CredentialException;
 
 @ControllerAdvice
 public class GlobalErrorHandler{
 
+    @ExceptionHandler(value = CredentialException.class)
+    public ResponseEntity<AuthenticationResult> handleException(Exception exception){
+        AuthenticationResult authenticationResult = new AuthenticationResult();
+        authenticationResult.setErrorMessage(exception.getMessage());
+        return ResponseEntity.status(400).body(authenticationResult);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<AuthenticationResult> handleExceptionGeneric(Exception exception){
+        AuthenticationResult authenticationResult = new AuthenticationResult();
+        authenticationResult.setErrorMessage(exception.getMessage());
+        return ResponseEntity.status(400).body(authenticationResult);
+    }
 }
